e62977f80c889f230947289c3cb655827c9d030b
GARRETT-PC\Garrett <garyin@uat.edu>
2019-04-18 22:00:42 -0700

    full project

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public int value;
+        public Node Left;
+        public Node Right;
+    }
+
+    class Tree
+    {
+        public Node insert(Node root, int valueTown)
+        {
+            if(root==null)
+            {
+                root = new Node();
+                root.value = valueTown;
+
+            }
+            else if(valueTown<root.value)
+            {
+                root.Left = insert(root.Left, valueTown);
+            }
+            else
+            {
+                root.Right = insert(root.Right, valueTown);
+            }
+            return root;
+        }
+
+        public void traverse(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            traverse(root.Left);
+            traverse(root.Right);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
